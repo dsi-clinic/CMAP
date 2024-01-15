2a5ed778358d8eba6cb28936d503ff04ce4fd52e
Spencer John Norman Ellis <sjne@fe01>
2024-01-15 14:58:00 -0600

    Improve code runtime and eliminate image duplication

--- utils/get_naip_images.py
+++ utils/get_naip_images.py
@@ -28,6 +28,22 @@ def get_geometry(fpath: str) -> Iterator[Dict[str, Any]]:
             yield {"id": feature.id, "geometry": feature["geometry"]}
 
 
+def get_catalog() -> pystac_client.Client:
+    """
+    Get the planetary computer catalog.
+
+    Returns
+    -------
+    pystac_client.Client
+        Pystac client for accessing the planetary computer catalog
+    """
+    catalog = pystac_client.Client.open(
+        "https://planetarycomputer.microsoft.com/api/stac/v1",
+        modifier=planetary_computer.sign_inplace,
+    )
+    return catalog
+
+
 def area_of_overlap(
     image_geom: Dict[str, Any], area_of_interest: Dict[str, Any]
 ) -> float:
@@ -56,18 +72,25 @@ def area_of_overlap(
     return overlap_area / target_area
 
 
-def get_image(
-    geometry: Dict[str, Any], date_range: str, img_type: str, save_dir: str
-) -> NoReturn:
+def get_image_url(
+    geometry: fiona.model.Geometry,
+    catalog: pystac_client.Client,
+    date_range: str,
+    img_type: str,
+) -> str:
     """
-    Retrieves NAIP imagery corresponding to an area of interest and writes it to disk.
-    If an area of interest covers more than one image, the image with the most overlap
-    is retrieved.
+    Retrieves NAIP image url corresponding to an area of interest.
+    If an area of interest covers more than one image, the url for the image with
+    the most overlap is retrieved.
 
     Parameters
     ----------
-    geometry : dict
-        Dictionary containing the id and geometry of the area of interest
+    geometry : fiona.model.Geometry
+        Similar to a GeoJSON, contains geometry data for shape from shapefile
+        corresponding to area of interest
+
+    catalog : pystac_client.Client
+        Planetary computer catalog to search for the image
 
     date_range : str
         Date range to search for imagery in the format "YYYY-MM-DD/YYYY-MM-DD"
@@ -77,34 +100,28 @@ def get_image(
             "rendered_preview" is a preview image in png format
             "image" is the full image in GeoTIFF format
 
-    save_dir : str
-        Directory to save the image to
+    Returns
+    -------
+    str
+        URL of the image corresponding to the area of interest
     """
     if img_type not in ["rendered_preview", "image"]:
         raise ValueError(
             "img_type must be either 'rendered_preview' or 'image'"
         )
 
-    # access the planetary computer catalog and retrieve NAIP images that
-    # intersect with the geometry
-    id = geometry["id"]
-    area_of_interest = geometry["geometry"]
-
-    catalog = pystac_client.Client.open(
-        "https://planetarycomputer.microsoft.com/api/stac/v1",
-        modifier=planetary_computer.sign_inplace,
-    )
+    # Search for NAIP images that intersect with the geometry
     search = catalog.search(
-        collections=["naip"], intersects=area_of_interest, datetime=date_range
+        collections=["naip"], intersects=geometry, datetime=date_range
     )
     items = search.item_collection()
 
     # if area of interest is not a point and more than one image is found,
     # get the image with the most overlap
-    if area_of_interest.type != "Point" and len(items) > 1:
+    if geometry.type != "Point" and len(items) > 1:
         max_overlap = sorted(
             items,
-            key=lambda x: area_of_overlap(x.geometry, dict(area_of_interest)),
+            key=lambda x: area_of_overlap(x.geometry, dict(geometry)),
             reverse=True,
         )[0]
         url = max_overlap.assets[img_type].href
@@ -112,6 +129,29 @@ def get_image(
     else:
         url = items[0].assets[img_type].href
 
+    return url
+
+
+def download_image(url: str, id: str, save_dir: str, img_type: str) -> NoReturn:
+    """
+    Downloads images from specified URL.
+
+    Parameters
+    ----------
+    url : str
+        URL of the image to be downloaded
+
+    id: str
+        Name/ID of the image to be used as file name
+
+    save_dir : str
+        Directory to save the image to
+
+    img_type : str
+        Type of image to retrieve. Options are "rendered_preview" and "image"
+            "rendered_preview" is a preview image in png format
+            "image" is the full image in GeoTIFF format
+    """
     # create the save path
     if img_type == "image":
         img_name = id + ".tif"
@@ -125,7 +165,9 @@ def get_image(
     # retrieve the image and write it to disk
     res = requests.get(url, stream=True)
     if res.status_code != 200:
-        f"Failed to download image: {res.text}. Status code: {res.status_code}"
+        print(
+            f"Failed to download image: {res.text}. Status code: {res.status_code}"
+        )
     else:
         print(f"Writing image {img_name} to disk")
 
@@ -134,19 +176,22 @@ def get_image(
             f.write(chunk)
 
 
-def main():
-    data_fpath = os.path.join(DATA_DIR, "GIBI_2021_shapefiles/GIBI_All.shp")
+def main(img_type: str, data_fpath: str, save_dir: str):
     geometry_stream = get_geometry(data_fpath)
-    save_dir = os.path.join(DATA_DIR, "GIBI-images")
+    catalog = get_catalog()
 
+    urls = set()
     for geometry in geometry_stream:
-        get_image(
-            geometry,
-            "2021-01-01/2024-01-01",
-            "image",
-            save_dir,
+        geom_id = geometry["id"]
+        url = get_image_url(
+            geometry["geometry"], catalog, "2021-01-01/2024-01-01", img_type
         )
+        if url not in urls:
+            urls.add(url)
+            download_image(url, geom_id, save_dir, img_type)
 
 
 if __name__ == "__main__":
-    main()
+    data_fpath = os.path.join(DATA_DIR, "GIBI_2021_shapefiles/GIBI_All.shp")
+    save_dir = os.path.join(DATA_DIR, "GIBI-images")
+    main("image", data_fpath, save_dir)
