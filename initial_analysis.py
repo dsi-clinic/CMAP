30754c78a4424664db951a8c5b2a15ba7c30b1aa
Tamami Tamura <tamamitamura@fe01>
2024-01-17 13:56:00 -0600

    open KC data and GIBI

---
+++ initial_analysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+fiona.open
+openfile file_name as data
+##
+shapefiles 
+kane county is gdb
+other is shp
+each feature has properties and geometries, GIBI - label & id
+geometries - type of geometry, GIBI is point with coordinates, and Kane are random polygons, points
+Kane county has layers within the gdb file, each layer corresponds to different type or label of what it is.
+
+##
+#fiona access what's in the data
