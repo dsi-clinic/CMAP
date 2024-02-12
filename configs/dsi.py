e47381fc7d18ece31ebb1550fa35c73358f48f2b
Matthew Rubenstein <43348193+Rubemat20@users.noreply.github.com>
2024-02-12 11:37:16 -0600

    Update dsi.py

--- configs/dsi.py
+++ configs/dsi.py
@@ -1,6 +1,6 @@
 import os
 
-DATA_ROOT = "/net/projects/cmap/data"
-KC_SHAPE_ROOT = os.path.join(DATA_ROOT, "kane-county-data")
-KC_IMAGE_ROOT = os.path.join(DATA_ROOT, "KC-images")
-KC_MASK_ROOT = os.path.join(DATA_ROOT, "KC-masks/top-structures-masks")
+DATA_DIR = "/net/projects/cmap/data"
+KC_SHAPE_DIR = os.path.join(DATA_DIR, "kane-county-data")
+KC_IMAGE_DIR = os.path.join(DATA_DIR, "KC-images")
+KC_MASK_DIR = os.path.join(DATA_DIR, "KC-masks/top-structures-masks")
