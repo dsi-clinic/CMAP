cadb028601969c609cb1004271bf9e7cd5269cd7
Tamami Tamura <tamamitamura@uchicago.edu>
2024-02-19 11:22:43 -0600

    testing loss functions

--- configs/dsi.py
+++ configs/dsi.py
@@ -5,11 +5,7 @@ DATA_ROOT = "/net/projects/cmap/data"
 KC_SHAPE_ROOT = os.path.join(DATA_ROOT, "kane-county-data")
 KC_IMAGE_ROOT = os.path.join(DATA_ROOT, "KC-images")
 KC_MASK_ROOT = os.path.join(DATA_ROOT, "KC-masks/top-structures-masks")
-<<<<<<< HEAD
-OUTPUT_ROOT = "/net/projects/cmap/tamami/weights"
-=======
 OUTPUT_ROOT = "/net/projects/cmap/tamami/loss_fn"
->>>>>>> loss_fn_testing
 
 # model selection
 MODEL = "unet"
@@ -18,11 +14,11 @@ EXTRA_CLASS = False
 WEIGHTS = True
 
 # model hyperparams
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 PATCH_SIZE = 256
 NUM_CLASSES = 5  # predicting 4 classes + background
 LR = 1e-3
-NUM_WORKERS = 2
-EPOCHS = 1
+NUM_WORKERS = 8
+EPOCHS = 5
 IGNORE_INDEX = 0  # index in images to ignore for jaccard index
-LOSS_FUNCTION = "JaccardLoss"  # JaccardLoss, DiceLoss, TverskyLoss, LovaszLoss
+LOSS_FUNCTION = "DiceLoss"  # JaccardLoss, DiceLoss, TverskyLoss, LovaszLoss
