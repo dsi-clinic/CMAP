c214cfbdb7601ae5fa5e6600e08e64de249f26a4
Miao Li <mli628@fe01>
2024-04-17 14:27:06 -0500

    update config with false bg

--- configs/dsi.py
+++ configs/dsi.py
@@ -4,8 +4,8 @@ import os
 DATA_ROOT = "/net/projects/cmap/data"
 KC_SHAPE_ROOT = os.path.join(DATA_ROOT, "kane-county-data")
 KC_IMAGE_ROOT = os.path.join(DATA_ROOT, "KC-images")
-KC_MASK_ROOT = os.path.join(DATA_ROOT, "KC-masks/top-structures-masks")
-OUTPUT_ROOT = "/net/projects/cmap/model-outputs"
+KC_MASK_ROOT = os.path.join(DATA_ROOT, "KC-masks/separate-masks")
+OUTPUT_ROOT = f"/net/projects/cmap/workspaces/{os.environ['USER']}"
 
 # model selection
 MODEL = "deeplabv3+"
