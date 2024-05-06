e66ba88fcc4dd0f0e5d81e09f9dd965b43c0f4bb
Miao Li <111526131+miaoli-04@users.noreply.github.com>
2024-05-06 10:38:31 -0500

    Update train.py

--- train.py
+++ train.py
@@ -26,7 +26,6 @@ from torch.utils.tensorboard import SummaryWriter
 from torchgeo.datasets import NAIP, random_bbox_assignment, stack_samples
 from torchmetrics import Metric
 from torchmetrics.classification import MulticlassJaccardIndex
-
 from data.kcv import KaneCounty
 from utils.model import SegmentationModel
 from utils.plot import find_labels_in_ground_truth, plot_from_tensors
