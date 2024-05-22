f8aea73111cf25f4be662b8f28ec6d491edfe5ca
Tamami Tamura <tamamitamura@uchicago.edu>
2024-05-22 18:05:32 -0500

    fixed model.py

--- model.py
+++ model.py
@@ -29,7 +29,6 @@ Exceptions:
 
 import importlib
 import os
-from typing import Union
 
 import segmentation_models_pytorch as smp
 from torchgeo.models import FCN, get_weight
@@ -58,66 +57,56 @@ class SegmentationModel:
         backbone for weights does not match the model backbone.
     """
 
-    def __init__(
-        self,
-        model: str = "unet",
-        backbone: str = "resnet50",
-        in_channels: int = 5,
-        num_classes: int = None,
-        weights: Union[WeightsEnum, str, bool] = True,
-    ):
+    def __init__(self, model_config):
         """
-        Code taken from torchgeo.models.SemanticSegmentationTask.configure_models
-
-        Configure the model with the provided params.
+        Initialize the SegmentationModel object with the provided model configuration.
 
         Parameters
         ----------
-        model : str
-            The model type to use. Options are 'unet', 'deeplabv3+', and 'fcn'.
+        model_config : dict
+            A dictionary containing configuration parameters for the model.
+            The dictionary should contain the following keys:
+                - "model": str, The model type to use. Options are
+                'unet', 'deeplabv3+', and 'fcn'.
+                - "backbone": str, The encoder to use, which is the classification
+                model that will be used to extract features. Options are
+                listed on the smp docs.
+                - "num_classes": int, The number of classes to predict. Should
+                match the number of classes in the mask.
+                - "weights": Union[str, bool], The weights to use for the model.
+                If True, uses imagenet weights. Can also accept a string path
+                to a weights file, or a WeightsEnum with pretrained weights.
 
-        backbone : str
-            The encoder to use, which is the classification model that will be used to
-            extract features. Options are listed on the smp docs:
-            <https://smp.readthedocs.io/en/latest/encoders.html>
-
-        in_channels : int
-            The number of input channels, i.e. the depth of the input image. For NAIP
-            data, this is 4.
-            When adding DEM data, this will be 5.
-            When using TorchGeo's pretrained weights, change appropriately and add
-            channels to data.
-
-        num_classes : int
-            The number of classes to predict. Should match the number of classes in
-            the mask.
-
-        weights : Union[str, bool]
-            The weights to use for the model. If True, uses imagenet weights. Can also
-            accept a string path to a weights file, or a WeightsEnum with pretrained
-            weights. Options for pretrained weights are listed on the torchgeo docs:
-            <https://torchgeo.readthedocs.io/en/stable/api/models.html#pretrained-weights>
+        Returns
+        -------
+        None
         """
+        model = model_config["model"]
+        self.backbone = model_config["backbone"]
+        self.num_classes = model_config["num_classes"]
+        self.weights = model_config["weights"]
+        self.in_channels = model_config.get("in_channels")
+        if self.in_channels is None:
+            self.in_channels = 5
         if model != "fcn":
+            state_dict = None
             # set custom weights
             # Assuming config.WEIGHTS contains the desired value,
             # e.g., "ResNet50_Weights.LANDSAT_TM_TOA_MOCO"
-            if weights and weights is not True:
-                weights_module, weights_submodule = weights.split(".")
-                imported_module = importlib.import_module("torchgeo.models")
-                # Get the attribute from the module
-                weights_class = getattr(imported_module, weights_module)
-                weights_attribute = getattr(weights_class, weights_submodule)
-                weights_meta = weights_attribute.meta
-                weights_chans = weights_meta["in_chans"]
-                del imported_module
+            if self.weights and self.weights is not True:
+                weights_module, weights_submodule = self.weights.split(".")
+                weights_attribute = getattr(
+                    importlib.import_module("torchgeo.models"),
+                    weights_module + "." + weights_submodule,
+                )
+                weights_chans = weights_attribute.meta["in_chans"]
 
-                in_channels = max(in_channels, weights_chans)
+                self.in_channels = max(self.in_channels, weights_chans)
 
                 weights_backbone = weights_module.split("_")[0]
                 if (
-                    weights_backbone.lower() != backbone
-                    and backbone != "vit_small_patch16_224"
+                    weights_backbone.lower() != self.backbone
+                    and self.backbone != "vit_small_patch16_224"
                 ):
                     raise ValueError(
                         "Backbone for weights does not match model backbone."
@@ -134,24 +123,26 @@ class SegmentationModel:
 
             if model == "unet":
                 self.model = smp.Unet(
-                    encoder_name=backbone,
-                    encoder_weights="swsl" if weights is True else None,
-                    in_channels=in_channels,
-                    classes=num_classes,
+                    encoder_name=self.backbone,
+                    encoder_weights="swsl" if self.weights is True else None,
+                    in_channels=self.in_channels,
+                    classes=self.num_classes,
                 )
 
             elif model == "deeplabv3+":
                 self.model = smp.DeepLabV3Plus(
-                    encoder_name=backbone,
-                    encoder_weights="imagenet" if weights is True else None,
-                    in_channels=in_channels,
-                    classes=num_classes,
+                    encoder_name=self.backbone,
+                    encoder_weights=(
+                        "imagenet" if self.weights is True else None
+                    ),
+                    in_channels=self.in_channels,
+                    classes=self.num_classes,
                 )
 
             elif model == "fcn":
                 self.model = FCN(
-                    in_channels=in_channels,
-                    classes=num_classes,
+                    in_channels=self.in_channels,
+                    classes=self.num_classes,
                     num_filters=3,
                 )
 
@@ -160,6 +151,18 @@ class SegmentationModel:
                     f"Model type '{model}' is not valid. "
                     "Currently, only supports 'unet', 'deeplabv3+' and 'fcn'."
                 )
-            if weights and weights is not True and model != "test_weights":
+            if self.weights and self.weights is not True:
                 self.model.encoder.load_state_dict(state_dict)
-            self.model.in_channels = in_channels
+            self.model.in_channels = self.in_channels
+
+    def __getbackbone__(self):
+        """
+        returns the backbone of the model
+        """
+        return self.backbone
+
+    def __getweights__(self):
+        """
+        returns the weights of the model
+        """
+        return self.weights
