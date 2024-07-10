4bd3bd8279b7c283b4c16159b5be0d4cf761181e
Ishita Jain <mailtoishita.jain@gmail.com>
2024-07-10 15:48:53 -0500

    test

--- configs/config.py
+++ configs/config.py
@@ -14,6 +14,7 @@ KC_DEM_ROOT = None
 KC_MASK_ROOT = os.path.join(DATA_ROOT, "KC-masks/separate-masks")
 OUTPUT_ROOT = f"/net/projects/cmap/workspaces/{os.environ['USER']}"
 
+print('hello')
 DIFFSAT_IMGS_ROOT = "/net/scratch/ijain1/naip_dataset"
 # this is where the images from generate image dataset is saved. if you want to change, edit at the bottom of the file when parsing argument into save_naip_images
 
