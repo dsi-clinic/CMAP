c87a5dfa79deb25abf3e9dbd427677563b5e0c9a
Collin Edward Kim <collinkim415@fe01.ds.uchicago.edu>
2024-11-03 22:49:43 -0600

    linting adjustments

--- train.py
+++ train.py
@@ -643,6 +643,10 @@ def test(
 
             # compute prediction error
             outputs = model(x)
+
+            if isinstance(outputs, tuple):
+                outputs = outputs[0]
+            
             loss = loss_fn(outputs, y_squeezed)
 
             # update metric
